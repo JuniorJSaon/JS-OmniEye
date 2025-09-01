d63dcbf63ccd992405daf5e9106818eb94f0b614
FiJay <filipszeliga441@gmail.com>
2025-09-01 13:10:24 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # JS-OmniEye
-One file debugger 
+One file debugger
+
+## Requrenments and how to use
+- at first install [**Tampermonkey**](https://www.tampermonkey.net/) in your browser
+- copy and save script into extention
+- enjoy the use
+
---
+++ userscript.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         JS-OmniEye
+// @namespace    http://tampermonkey.net/
+// @version      2025-09-01
+// @description  One file debugger, made with much love
+// @author       JSaon
+// @run-at       document-start
+// @include      *
+// @match        *://*/*
+// @match        https://*/*
+// @match        http://*/*
+// @icon         https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThUUqw5jj4HYkQ56R0JOJZL9hztGN10jHmrA&s
+// @grant        none
+// ==/UserScript==
+
+console.info("JS-OmniEye is loading...");
